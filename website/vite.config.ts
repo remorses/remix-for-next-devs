25d0b4cee28628cfb497700031c24d960a1d290c
remorses <beats.by.morse@gmail.com>
2024-05-03 19:06:15 +0200

    website: use mdx

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -10,6 +10,7 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+    
     plugins: [
         remixCloudflareDevProxy(),
         mdx({
